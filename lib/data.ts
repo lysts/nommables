2de97610cda2d155684028222853c429c97108f6
Lyla <123667606+lysts@users.noreply.github.com>
2024-03-27 16:13:08 +0000

    make new app setup for nommables

---
+++ lib/data.ts
@@ -0,0 +1,26 @@
+export const links = [
+    {
+      name: "Home",
+      hash: "#home",
+    },
+    {
+      name: "About",
+      hash: "#about",
+    },
+    {
+      name: "Recipes",
+      hash: "#recipes",
+    },
+    {
+      name: "Add item",
+      hash: "#additem",
+    },
+    {
+      name: "Search",
+      hash: "#search",
+    },
+    {
+      name: "Profile",
+      hash: "#profile",
+    },
+  ] as const;
